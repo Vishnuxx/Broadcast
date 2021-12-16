0be7336dac786cb89b243229badf0d1a671de138
vishnuxx <vishnuvs2002it@gmail.com>
2021-12-16 20:32:45 +0530

    added enable disable functionality

--- Broadcast.js
+++ Broadcast.js
@@ -1,31 +1,30 @@
-
-
 function Broadcast() {
-    var handlers = [];
-    var enabled = true;
- 
-    this.register = (callback) => {
-        if(callback instanceof Function){
-          handlers.push(callback);
-        }else{
-         throw "Can register only callback functions";
-        }
-        
+  var handlers = [];
+  var enabled = true;
+
+  this.register = (callback) => {
+    if (callback instanceof Function) {
+      handlers.push(callback);
+    } else {
+      throw "Can register only callback functions";
     }
- 
-    this.enable = () => {
-        enabled = true;
+  };
+
+  this.enable = () => {
+    enabled = true;
+  };
+
+  this.disable = () => {
+    enabled = false;
+  };
+
+  this.dispatch = (...args) => {
+    if (enabled) {
+      for (let callback of handlers) {
+        callback(...args);
+      }
     }
- 
-    this.disable = () => {
-        enabled = false;
-    }
- 
-    this.dispatch = (...args) => {
-        for(let callback of handlers) {
-            callback(...args);
-        }
-    }
- }
- 
- export default Broadcast;
+  };
+}
+
+export default Broadcast;
